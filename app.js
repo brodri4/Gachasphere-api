0ae036f76468e95058b04942aadc8b2386e276c3
Tran Hung <tran.hung0713@gmail.com>
2020-12-17 12:50:05 -0600

    adding bcrypt

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const app = express();
 const models = require("./models");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 app.use(cors());
 app.use(express.json());
+
+const SALT_ROUNDS = 10;
 
 app.post("/login", async (req, res) => {
   const username = req.body.username;
@@ -16,15 +19,20 @@ app.post("/login", async (req, res) => {
   const persistedUser = await models.User.findOne({
     where: {
       username: username,
-      password: password,
     },
   });
   if (persistedUser) {
-    var token = jwt.sign(
-      { username: username, userId: persistedUser.id },
-      process.env.TK_PASS
-    );
-    res.json({ token: token });
+    bcrypt.compare(password, persistedUser.password, (error, result) => {
+      if (result) {
+        var token = jwt.sign(
+          { username: username, userId: persistedUser.id },
+          process.env.TK_PASS
+        );
+        res.json({ token: token });
+      } else {
+        res.json({ message: "Invalid username or password" });
+      }
+    });
   } else {
     res.json({ message: "Invalid username or password" });
   }
@@ -35,12 +43,27 @@ app.post("/register", async (req, res) => {
   const password = req.body.password;
   const email = req.body.email;
 
-  const user = models.User.build({
-    username: username,
-    password: password,
-    email: email,
+  const persistedUser = await models.User.findOne({
+    where: {
+      username: username,
+    },
   });
-  user.save().then((result) => res.json({ success: true }));
+  if (!persistedUser) {
+    bcrypt.hash(password, SALT_ROUNDS, async (error, hash) => {
+      if (error) {
+        res.json({ message: "Cannot create user!" });
+      } else {
+        const user = await models.User.build({
+          username: username,
+          password: hash,
+          email: email,
+        });
+        user.save().then((result) => res.json({ success: true }));
+      }
+    });
+  } else {
+    res.json({ message: "User already exists!" });
+  }
 });
 
 //This is for testing only
