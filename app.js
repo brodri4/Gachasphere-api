31f7bb48518ad2380ed4829b4827b02604f3bd63
Tran Hung <tran.hung0713@gmail.com>
2020-12-17 11:28:48 -0600

    adding login and register

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-require('dotenv').config()
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const models = require("./models");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const persistedUser = await models.User.findOne({
+    where: {
+      username: username,
+      password: password,
+    },
+  });
+  if (persistedUser) {
+    var token = jwt.sign(
+      { username: username, userId: persistedUser.id },
+      "MYSECRET125"
+    );
+    res.json({ token: token });
+  } else {
+    res.json({ message: "Invalid username or password" });
+  }
+});
+
+app.post("/register", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+
+  const user = models.User.build({
+    username: username,
+    password: password,
+    email: email,
+  });
+  user.save().then((result) => res.json({ success: true }));
+});
+
+//This is for testing only
+
+app.get("/hello", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(8080, () => {
+  console.log("Server is Running");
+});
--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
-  "development": {
-    "username": "isafbjli",
-    "password": process.env.PW,
-    "database": "isafbjli",
-    "host": "suleiman.db.elephantsql.com",
-    "dialect": "postgres"
+  development: {
+    username: "isafbjli",
+    password: process.env.PW,
+    database: "isafbjli",
+    host: "suleiman.db.elephantsql.com",
+    dialect: "postgres",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": "isafbjli",
-    "password": process.env.PW,
-    "database": "isafbjli",
-    "host": "suleiman.db.elephantsql.com",
-    "dialect": "postgres"
-  }
-}
+  production: {
+    username: "isafbjli",
+    password: process.env.PW,
+    database: "isafbjli",
+    host: "suleiman.db.elephantsql.com",
+    dialect: "postgres",
+  },
+};
