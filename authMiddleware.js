aa78439de10e11e9470daf071f5a579ef350509e
abbeyperini <abbeyperini@gmail.com>
2021-01-04 11:46:14 -0500

    authMiddleware headers if else

--- authMiddleware.js
+++ authMiddleware.js
@@ -3,30 +3,36 @@ const models = require("./models");
 require("dotenv").config();
 
 async function authentication(req, res, next) {
-  let headers = req.headers["authorization"];
-  const token = headers.split(" ")[1];
+  let token = '';
 
-  if (token) {
-    const decoded = jwt.verify(token, process.env.TK_PASS);
-    if (decoded) {
-      const username = decoded.username;
-      const userId = decoded.userId;
-      const persistedUser = await models.User.findOne({
-        where: {
-          username: username,
-        },
-      });
-      if (persistedUser) {
-        res.locals.user = { userId: userId };
-        next();
+  if (req.headers) {
+    let headers = req.headers["authorization"];
+    token = headers.split(" ")[1];
+
+    if (token) {
+      const decoded = jwt.verify(token, process.env.TK_PASS);
+      if (decoded) {
+        const username = decoded.username;
+        const userId = decoded.userId;
+        const persistedUser = await models.User.findOne({
+          where: {
+            username: username,
+          },
+        });
+        if (persistedUser) {
+          res.locals.user = { userId: userId };
+          next();
+        } else {
+          res.json({ message: "You're not authorized!" });
+        }
       } else {
-        res.json({ message: "You're not authorized!" });
+        res.json({ message: "You're not authorized!", error: "database" });
       }
     } else {
-      res.json({ message: "You're not authorized!" });
+      res.json({ message: "You're not authorized!", error: "jwt verify" });
     }
   } else {
-    res.json({ message: "You're not authorized!" });
+    res.json({ message: "You're not authorized!", error: "headers"});
   }
 }
 
